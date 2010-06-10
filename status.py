0d74cd9c5c8d5374fbc82a27231ce6aa6fbbc4a7
buildbot.net Trac <buildbot@buildbot.net>
2010-06-10 17:53:06 -0500

    add failureToSuccess to IRC notifications

--- status.py
+++ status.py
@@ -20,6 +20,7 @@ from buildbot.status import words
 status.append(words.IRC(host="irc.freenode.net", nick="metabbot",
 				notify_events={
 					'successToFailure' : 1,
+					'failureToSuccess' : 1,
 				},
                               channels=["#buildbot"]))
 
