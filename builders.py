5c1180f5275773256021431123ae79b10a4d1192
Tom Prince <tom.prince@ualberta.net>
2012-02-09 19:08:25 -0500

    Properly configure sqlalchemy builder.s

--- builders.py
+++ builders.py
@@ -340,9 +340,9 @@ python_versions = dict(
     py24='python2.4',
     py25='python2.5',
     py26='python2.6',
-    py27='python2.7',
     pypy17='pypy1.7',
     pypy18='pypy1.8',
+    py27='python2.7', ## This is last, so that sqlalchemy tests uses this version of python
 )
 
 for py, python_version in python_versions.items():
