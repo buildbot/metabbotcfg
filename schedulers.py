8e5d71d3f1ff03602299467f055947b69a6c6b6c
Tom Prince <tom.prince@ualberta.net>
2012-02-09 19:04:36 -0500

    Add ForceScheduler to config.

--- schedulers.py
+++ schedulers.py
@@ -1,6 +1,8 @@
 schedulers = []
 
 from buildbot.schedulers.basic import Scheduler
+from buildbot.schedulers.forcesched import ForceScheduler
+
 from buildbot.schedulers.timed import Periodic
 from buildbot.schedulers.trysched import Try_Userpass
 from metabbotcfg.slaves import slaves
@@ -9,3 +11,5 @@ from metabbotcfg.builders import builders
 schedulers.append(Scheduler(name="all", branch='master',
                                  treeStableTimer=10,
                                  builderNames=[ b['name'] for b in builders ]))
+schedulers.append(ForceScheduler(name="force",
+    builderNames=[ b['name'] for b in builders ]))
