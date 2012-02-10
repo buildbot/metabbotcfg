898b006efb02212a650a7dac70b19da89cf7704b
Tom Prince <tom.prince@ualberta.net>
2012-02-10 12:31:39 -0500

    Simplify force build form.
    We don't use properties or project, and don't allow changing the repo,
    so don't show those fields. Also, specify default branch, so that the
    grid view for force builds corresponds to triggered builds.

--- schedulers.py
+++ schedulers.py
@@ -1,7 +1,7 @@
 schedulers = []
 
 from buildbot.schedulers.basic import Scheduler
-from buildbot.schedulers.forcesched import ForceScheduler
+from buildbot.schedulers.forcesched import ForceScheduler, FixedParameter, StringParameter
 
 from buildbot.schedulers.timed import Periodic
 from buildbot.schedulers.trysched import Try_Userpass
@@ -12,4 +12,8 @@ schedulers.append(Scheduler(name="all", branch='master',
                                  treeStableTimer=10,
                                  builderNames=[ b['name'] for b in builders ]))
 schedulers.append(ForceScheduler(name="force",
+    repository=FixedParameter(name="repository", default='git://github.com/buildbot/buildbot.git'),
+    branch=StringParameter(name="branch", default="master"),
+    project=FixedParameter(name="project", default=""),
+    properties=[],
     builderNames=[ b['name'] for b in builders ]))
