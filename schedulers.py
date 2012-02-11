5a5e707dc011b95b83abac70e31b654501a5d274
buildbot.net admins <buildbot-devel@lists.sourceforge.net>
2012-02-11 18:12:44 +0100

    Don't use stale builders info to build schedulers' builderNames

--- schedulers.py
+++ schedulers.py
@@ -6,14 +6,14 @@ from buildbot.schedulers.forcesched import ForceScheduler, FixedParameter, Strin
 from buildbot.schedulers.timed import Periodic
 from buildbot.schedulers.trysched import Try_Userpass
 from metabbotcfg.slaves import slaves
-from metabbotcfg.builders import builders
+from metabbotcfg import builders
 
 schedulers.append(Scheduler(name="all", branch='master',
                                  treeStableTimer=10,
-                                 builderNames=[ b['name'] for b in builders ]))
+                                 builderNames=[ b['name'] for b in builders.builders ]))
 schedulers.append(ForceScheduler(name="force",
     repository=FixedParameter(name="repository", default='git://github.com/buildbot/buildbot.git'),
     branch=StringParameter(name="branch", default="master"),
     project=FixedParameter(name="project", default=""),
     properties=[],
-    builderNames=[ b['name'] for b in builders ]))
+    builderNames=[ b['name'] for b in builders.builders ]))
