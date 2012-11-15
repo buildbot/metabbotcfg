1bdef29d7557de291ef1345e20e42852907d795f
Berkus <berkus@gmail.com>
2012-11-15 21:23:50 +0200

    Fix typo.

--- builders.py
+++ builders.py
@@ -367,7 +367,7 @@ for sl in get_slaves(run_single=True).values():
 
 for opsys in set(sl.os for sl in slaves if sl.os is not None):
     if 'win' in opsys:
-        test_master = 'cgywin' in opsys
+        test_master = 'cygwin' in opsys
         f = mksimplefactory(test_master=test_master)
     else:
         f = mktestfactory()
