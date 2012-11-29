bb79fbc744e47c13e1de6bce02c8ce220dc599f9
buildbot.net admins <buildbot-devel@lists.sourceforge.net>
2012-11-29 22:22:17 +0100

    remove nine scheduler temporarily

--- schedulers.py
+++ schedulers.py
@@ -18,9 +18,9 @@ schedulers.append(SingleBranchScheduler(name="release", branch='buildbot-0.8.7',
                                  treeStableTimer=10,
                                  builderNames=[ b['name'] for b in builders.master_builders ]))
 
-schedulers.append(SingleBranchScheduler(name="nine", branch='nine',
-                    treeStableTimer=5,
-                    builderNames=[ b['name'] for b in builders.nine_builders ]))
+#schedulers.append(SingleBranchScheduler(name="nine", branch='nine',
+#                    treeStableTimer=5,
+#                    builderNames=[ b['name'] for b in builders.nine_builders ]))
 
 schedulers.append(ForceScheduler(name="force",
     repository=FixedParameter(name="repository", default='git://github.com/buildbot/buildbot.git'),
