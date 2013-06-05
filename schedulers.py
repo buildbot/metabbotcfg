0074b7f95d823c2c263b79ebaa00c50d7e6d5f13
buildbot.net admins <buildbot-devel@lists.sourceforge.net>
2013-06-05 21:38:39 +0200

    re-enable nine, build 0.8.8.

--- schedulers.py
+++ schedulers.py
@@ -14,13 +14,13 @@ schedulers.append(SingleBranchScheduler(name="all", branch='master',
                                  treeStableTimer=10,
                                  builderNames=[ b['name'] for b in builders.master_builders ]))
 
-schedulers.append(SingleBranchScheduler(name="release", branch='buildbot-0.8.7',
+schedulers.append(SingleBranchScheduler(name="release", branch='buildbot-0.8.8',
                                  treeStableTimer=10,
                                  builderNames=[ b['name'] for b in builders.master_builders ]))
 
-#schedulers.append(SingleBranchScheduler(name="nine", branch='nine',
-#                    treeStableTimer=5,
-#                    builderNames=[ b['name'] for b in builders.nine_builders ]))
+schedulers.append(SingleBranchScheduler(name="nine", branch='nine',
+                    treeStableTimer=5,
+                    builderNames=[ b['name'] for b in builders.nine_builders ]))
 
 schedulers.append(ForceScheduler(name="force",
     repository=FixedParameter(name="repository", default='git://github.com/buildbot/buildbot.git'),
