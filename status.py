d9485511d2ef7131b6b69d5395fce26a59d79b11
Mikhail Sobolev <mss@mawhrin.net>
2014-12-05 20:43:14 +0200

    disable IRC for nine
    it currently failes (see ticket:3094).

--- status.py
+++ status.py
@@ -1,9 +1,11 @@
 status = []
 
 from buildbot.status import words
-status.append(words.IRC(host="irc.freenode.net", nick="bb-9-meta",
-				notify_events={
-					'successToFailure' : 1,
-					'failureToSuccess' : 1,
-				},
-                channels=["#buildbot"]))
+if 0:
+    status.append(words.IRC(host="irc.freenode.net",
+                            nick="bb-9-meta",
+                            notify_events={
+                                'successToFailure' : 1,
+                                'failureToSuccess' : 1,
+                            },
+                            channels=["#buildbot"]))
