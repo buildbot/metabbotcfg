2fb2ec64f99e53a9aaebd5142fc5d004ea2d785b
Mikhail Sobolev <mss@mawhrin.net>
2014-12-13 19:34:53 +0200

    get rid of tabs

--- status.py
+++ status.py
@@ -6,23 +6,23 @@ from buildbot.status.web.auth import BasicAuth
 
 users = [ ('dev', 'bbot!')] # it's not *that* secret..
 authz = Authz(auth=BasicAuth(users),
-	forceBuild='auth',
+        forceBuild='auth',
     forceAllBuilds='auth',
 )
 status.append(html.WebStatus(
-		http_port=8010,
-		authz=authz,
-		order_console_by_time=True,
-		revlink="http://github.com/buildbot/buildbot/commit/%s",
-		changecommentlink=(r'\b#(\d+)\b', r'http://buildbot.net/trac/ticket/\1',
-				   r'Ticket \g<0>'),
-		change_hook_dialects={ 'github' : True },
-		))
+                http_port=8010,
+                authz=authz,
+                order_console_by_time=True,
+                revlink="http://github.com/buildbot/buildbot/commit/%s",
+                changecommentlink=(r'\b#(\d+)\b', r'http://buildbot.net/trac/ticket/\1',
+                                   r'Ticket \g<0>'),
+                change_hook_dialects={ 'github' : True },
+                ))
 
 from buildbot.status import words
 status.append(words.IRC(host="irc.freenode.net", nick="bb-meta",
-				notify_events={
-					'successToFailure' : 1,
-					'failureToSuccess' : 1,
-				},
+                                notify_events={
+                                        'successToFailure' : 1,
+                                        'failureToSuccess' : 1,
+                                },
                 channels=["#buildbot"]))
