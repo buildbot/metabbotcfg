68da86bf86c96d6708c26c333b9556f0a1a09be0
Mikhail Sobolev <mss@mawhrin.net>
2015-01-06 15:45:12 +0200

    convert schedulers.py to use plugins

--- schedulers.py
+++ schedulers.py
@@ -1,21 +1,19 @@
-from buildbot.schedulers.basic import SingleBranchScheduler
-from buildbot.schedulers.forcesched import ForceScheduler, FixedParameter, StringParameter, ChoiceStringParameter
-from buildbot.schedulers.timed import Periodic
-from buildbot.schedulers.trysched import Try_Userpass
+from buildbot.plugins import scheduler
+from buildbot.plugins import util
 
 from metabbotcfg import builders
 
 schedulers = [
-    SingleBranchScheduler(name="all", branch='master',
-                          treeStableTimer=2,
-                          builderNames=[b['name'] for b in builders.builders]),
+    scheduler.SingleBranchScheduler(name="all", branch='master',
+                                    treeStableTimer=2,
+                                    builderNames=[b['name'] for b in builders.builders]),
 #   SingleBranchScheduler(name="release", branch='buildbot-0.8.9',
 #                         treeStableTimer=10,
 #                         builderNames=[b['name'] for b in builders.builders if b['name'] not in ('docs',)])),
-    ForceScheduler(name="force",
-                   codebases=[''], # ?? {'repository': FixedParameter(name="repository", default='git://github.com/buildbot/buildbot.git')},
-                   #branch=ChoiceStringParameter(name="branch", default="master", choices=["master", "eight"]),
-                   #project=FixedParameter(name="project", default=""),
-                   properties=[],
-                   builderNames=[b['name'] for b in builders.builders])
+    scheduler.ForceScheduler(name="force",
+                             codebases=[''], # ?? {'repository': util.FixedParameter(name="repository", default='git://github.com/buildbot/buildbot.git')},
+                             #branch=util.ChoiceStringParameter(name="branch", default="master", choices=["master", "eight"]),
+                             #project=util.FixedParameter(name="project", default=""),
+                             properties=[],
+                             builderNames=[b['name'] for b in builders.builders])
 ]
