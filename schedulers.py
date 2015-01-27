1f82fdba6fd40cc013b700033bb44e7b8afdabd9
Mikhail Sobolev <mss@mawhrin.net>
2015-01-27 13:34:27 +0200

    fix schedudlers name

--- schedulers.py
+++ schedulers.py
@@ -1,19 +1,19 @@
-from buildbot.plugins import scheduler
+from buildbot.plugins import schedulers as bbsched
 from buildbot.plugins import util
 
 from metabbotcfg import builders
 
 schedulers = [
-    scheduler.SingleBranchScheduler(name="all", branch='master',
-                                    treeStableTimer=2,
-                                    builderNames=[b['name'] for b in builders.builders]),
+    bbsched.SingleBranchScheduler(name="all", branch='master',
+                                  treeStableTimer=2,
+                                  builderNames=[b['name'] for b in builders.builders]),
 #   SingleBranchScheduler(name="release", branch='buildbot-0.8.9',
 #                         treeStableTimer=10,
 #                         builderNames=[b['name'] for b in builders.builders if b['name'] not in ('docs',)])),
-    scheduler.ForceScheduler(name="force",
-                             codebases=[''], # ?? {'repository': util.FixedParameter(name="repository", default='git://github.com/buildbot/buildbot.git')},
-                             #branch=util.ChoiceStringParameter(name="branch", default="master", choices=["master", "eight"]),
-                             #project=util.FixedParameter(name="project", default=""),
-                             properties=[],
-                             builderNames=[b['name'] for b in builders.builders])
+    bbsched.ForceScheduler(name="force",
+                           codebases=[''], # ?? {'repository': util.FixedParameter(name="repository", default='git://github.com/buildbot/buildbot.git')},
+                           #branch=util.ChoiceStringParameter(name="branch", default="master", choices=["master", "eight"]),
+                           #project=util.FixedParameter(name="project", default=""),
+                           properties=[],
+                           builderNames=[b['name'] for b in builders.builders])
 ]
