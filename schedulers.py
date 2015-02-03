b051e9f7b1f3732b3007ea81fc68a1abd3095372
Mikhail Sobolev <mss@mawhrin.net>
2015-02-03 22:15:37 +0200

    cosmetics for schedulers.py

--- schedulers.py
+++ schedulers.py
@@ -4,16 +4,21 @@ from buildbot.plugins import util
 from metabbotcfg import builders
 
 schedulers = [
-    bbsched.SingleBranchScheduler(name="all", branch='master',
-                                  treeStableTimer=2,
-                                  builderNames=[b['name'] for b in builders.builders]),
-#   SingleBranchScheduler(name="release", branch='buildbot-0.8.9',
-#                         treeStableTimer=10,
-#                         builderNames=[b['name'] for b in builders.builders if b['name'] not in ('docs',)])),
-    bbsched.ForceScheduler(name="force",
-                           codebases=[''], # ?? {'repository': util.FixedParameter(name="repository", default='git://github.com/buildbot/buildbot.git')},
-                           #branch=util.ChoiceStringParameter(name="branch", default="master", choices=["master", "eight"]),
-                           #project=util.FixedParameter(name="project", default=""),
-                           properties=[],
-                           builderNames=[b['name'] for b in builders.builders])
+    bbsched.SingleBranchScheduler(
+        name="all",
+        branch='master',
+        treeStableTimer=2,
+        builderNames=[b['name'] for b in builders.builders]),
+#   SingleBranchScheduler(
+#       name="release",
+#       branch='buildbot-0.8.9',
+#       treeStableTimer=10,
+#       builderNames=[b['name'] for b in builders.builders if b['name'] not in ('docs',)])),
+    bbsched.ForceScheduler(
+        name="force",
+        codebases=[''], # ?? {'repository': util.FixedParameter(name="repository", default='git://github.com/buildbot/buildbot.git')},
+        #branch=util.ChoiceStringParameter(name="branch", default="master", choices=["master", "eight"]),
+        #project=util.FixedParameter(name="project", default=""),
+        properties=[],
+        builderNames=[b['name'] for b in builders.builders])
 ]
