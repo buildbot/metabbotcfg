871c1ccb2abd99d4ad41753f5020eb68f3c78bc7
Mikhail Sobolev <mss@mawhrin.net>
2015-07-18 19:45:13 +0300

    disable IRC _status_ target for now

--- status.py
+++ status.py
@@ -1,12 +1,14 @@
-from buildbot.plugins import status as status_targets
+# XXX(sa2ajj): make use of IRC _reporter_ when it's available
 
-status = []
+# from buildbot.plugins import status as status_targets
+#
+# status = []
 
-if 0:
-    status.append(status_targets.IRC(host="irc.freenode.net",
-                                     nick="bb-9-meta",
-                                     notify_events={
-                                         'successToFailure' : 1,
-                                         'failureToSuccess' : 1,
-                                     },
-                                     channels=["#buildbot"]))
+# if 0:
+#     status.append(status_targets.IRC(host="irc.freenode.net",
+#                                      nick="bb-9-meta",
+#                                      notify_events={
+#                                          'successToFailure' : 1,
+#                                          'failureToSuccess' : 1,
+#                                      },
+#                                      channels=["#buildbot"]))
