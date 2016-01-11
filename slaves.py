65885f82f4b0d42b45b3969e9bd69d4326d3d44f
Dustin J. Mitchell <dustin@mozilla.com>
2016-01-11 01:55:39 +0000

    add a postgresql db config for linux, too

--- slaves.py
+++ slaves.py
@@ -130,6 +130,8 @@ slaves = [
         py27=True,
         pyqt4=False,
         databases={
+            'postgres' : dict(BUILDBOT_TEST_DB_URL=
+                'postgresql+pg8000://${POSTGRESQL_ENV_POSTGRES_USER}:${POSTGRESQL_ENV_POSTGRES_PASSWORD}@${POSTGRESQL_PORT_5432_TCP_ADDR}:${POSTGRESQL_PORT_5432_TCP_PORT}/${POSTGRESQL_ENV_POSTGRES_USER}'),
             'mysql' : dict(BUILDBOT_TEST_DB_URL=
                 "mysql+mysqldb://${MYSQL_ENV_MYSQL_USER}:${MYSQL_ENV_MYSQL_PASSWORD}@${MYSQL_PORT_3306_TCP_ADDR}:${MYSQL_PORT_3306_TCP_PORT}/${MYSQL_ENV_MYSQL_DATABASE}"),
         }
