4e4495f8b1582772600390b0f13300e8618e8f2f
Tom Prince <tom.prince@twistedmatrix.com>
2016-05-11 15:47:28 -0600

    Also check config.

--- slaves.py
+++ slaves.py
@@ -58,6 +58,8 @@ class MySlaveBase(object):
         return remaining
 
     def get_pass(self, name):
+        if os.environ['CHECK_CONFIG'] == 'true':
+            return ''
         # get the password based on the name
         path = os.path.join(os.path.dirname(__file__), "%s.pass" % name)
         pw = open(path).read().strip()
