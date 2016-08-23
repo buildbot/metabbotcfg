800f4c9759e1ac34a732a15c435f5b0ce1906ec8
Vladimir Rutsky <rutsky@users.noreply.github.com>
2016-08-23 22:15:11 +0400

    remove trailing space

--- builders.py
+++ builders.py
@@ -382,7 +382,7 @@ database_packages = {
 }
 
 for db in set(itertools.chain.from_iterable(sl.databases.keys() for sl in slaves)):
-    f = mktestfactory(extra_packages=database_packages[db], 
+    f = mktestfactory(extra_packages=database_packages[db],
                       db=db,
                       python_version='python2.7')
     builders.append({
