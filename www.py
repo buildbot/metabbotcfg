d488770a24225830977e9640f8b639bc07097760
Pierre Tardy <tardyp@gmail.com>
2016-09-13 16:37:03 +0200

    configure buildbot travis to use hyper and github oauth
    this is associated to an ansible commit which provision two .pass files containing the creds

--- www.py
+++ www.py
@@ -1,3 +1,5 @@
+import json
+import os
 import platform
 
 
@@ -5,4 +7,8 @@ def setupWWW(c):
     if platform.node() == 'nine':
         c['www']['port'] = 'tcp:8010:interface=192.168.80.244'
     c['www']['plugins']['waterfall_view'] = {}
-    c['www']['change_hook_dialects'] = {'github': {}}
+
+    # read the password that ansible did sent to us, and override what is in the yaml
+    creds = json.load(open(os.path.join(os.path.dirname(__file__), "github_oauth.pass")))
+    c['www']['auth'].clientId = creds['client_id']
+    c['www']['auth'].clientSecret = creds['client_secret']
