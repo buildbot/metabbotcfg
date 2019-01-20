1f7b0874ae52cf49abb96c6a1de2976729fbcb52
Pierre Tardy <tardyp@gmail.com>
2019-01-20 15:11:59 +0100

    we have 7 CPUs on the kube cluster, we can place up to 7/.7 = 10 builds at the same time

--- workers.py
+++ workers.py
@@ -184,7 +184,7 @@ else:
             defer.returnValue(True)
 
 workers = [
-    # add 7 kube workers
+    # add 10 kube workers
     MyKubeWorker(
         'kube{:02d}'.format(i),
         max_builds=1,
@@ -193,7 +193,7 @@ workers = [
         run_config=True,
         py26=True,
         py27=True)
-    for i in range(6)
+    for i in range(10)
 ] + [
     # add 4 local workers
     MyLocalWorker(
